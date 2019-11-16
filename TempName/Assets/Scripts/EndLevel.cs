2a3af30fd5ee6f84637867925efd62c0304afc7d
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-11-16 12:15:57 +0100

    level 2 and switch scenes in scene end

---
+++ TempName/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    public string sceneName;
+    //private BoxCollider2D endLevelCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //endLevelCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Player entered end level collider");
+
+            // Todo?:Save player stats.
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
