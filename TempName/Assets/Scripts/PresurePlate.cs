121bdacbf20429a2b852739739bc684c6d7e469d
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-11-16 18:26:59 +0100

    Pressure plate sprites

--- TempName/Assets/Scripts/PresurePlate.cs
+++ TempName/Assets/Scripts/PresurePlate.cs
@@ -8,15 +8,33 @@ public class PresurePlate : MonoBehaviour
     public UnityEvent onPressed;
     public UnityEvent onRelease;
 
+    public Sprite pressed;
+    private Sprite released;
+    private SpriteRenderer render;
+
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        released = render.sprite;
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("DynamicObject"))
+        {
             onPressed.Invoke();
+            render.sprite = pressed;
+        }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("DynamicObject"))
+        {
             onRelease.Invoke();
+            render.sprite = released;
+        }
     }
 }
