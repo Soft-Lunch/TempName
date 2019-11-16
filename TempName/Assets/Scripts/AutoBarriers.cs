fc9226337e302d7a36e3a48db89bb91d06e21950
Aleix Gabarró <aleix.gab@gmail.com>
2019-11-16 17:37:20 +0100

    AutoBarrier

---
+++ TempName/Assets/Scripts/AutoBarriers.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBarriers : MonoBehaviour
+{
+    public bool nextUp;
+    public float speed;
+
+    public float timeWaiting = 1.0f;
+    public float timeMooving = 1.0f;
+
+    float timeCont = 0.0f;
+    bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCont = 0.0f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeCont >= timeWaiting && !isMoving)
+        {
+            isMoving = true;
+            timeCont = 0.0f;
+        }
+
+        if (timeCont >= timeMooving && isMoving)
+        {
+            nextUp = !nextUp;
+            isMoving = false;
+            timeCont = 0.0f;
+        }
+
+        Debug.Log(timeCont);
+        if (isMoving)
+        {
+
+            if (nextUp)
+            {
+                float posY = transform.position.y + (speed * Time.deltaTime);
+                transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+            }
+            else
+            {
+                float posY = transform.position.y - (speed * Time.deltaTime);
+                transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+            }
+        }
+
+        timeCont += Time.deltaTime;
+    }
+}
