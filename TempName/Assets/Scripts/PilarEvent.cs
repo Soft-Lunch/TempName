e9a5bd9e696867786d79477eecd0e1b39f4b5270
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-11-17 05:49:59 +0100

    Update PilarEvent.cs

--- TempName/Assets/Scripts/PilarEvent.cs
+++ TempName/Assets/Scripts/PilarEvent.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PilarEvent: MonoBehaviour
+public class PilarEvent : MonoBehaviour
 {
+    public bool freezePlayer = false;
+
+    public SpongeBehavior sponge;
+    public RockyBehavior rocky;
+    public LiamBehavior liam;
+
     bool goingUp = false;
     bool goingDown = false;
 
@@ -19,7 +25,7 @@ public class PilarEvent: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(goingDown)
+        if (goingDown)
         {
             float posY = transform.position.y - (velocity * Time.deltaTime);
             transform.position = new Vector3(transform.position.x, posY, transform.position.z);
@@ -27,31 +33,62 @@ public class PilarEvent: MonoBehaviour
             {
                 transform.position = new Vector3(transform.position.x, posMinY, transform.position.z);
                 goingDown = false;
+
+                StartCoroutine(MovePlayer());
+
             }
 
         }
-        else if(goingUp)
+        else if (goingUp)
         {
             float posY = transform.position.y + (velocity * Time.deltaTime);
             transform.position = new Vector3(transform.position.x, posY, transform.position.z);
-            if(transform.position.y >= posMaxY)
+            if (transform.position.y >= posMaxY)
             {
                 transform.position = new Vector3(transform.position.x, posMaxY, transform.position.z);
                 goingUp = false;
+
+                StartCoroutine(MovePlayer());
             }
         }
     }
+
+    private IEnumerator MovePlayer()
+    {
+        yield return new WaitForSeconds(1);
+
+        if (freezePlayer)
+        {
+            sponge.stop = false;
+            rocky.stop = false;
+            liam.stop = false;
+        }
+    }
+
+
 
     public void PilarDown()
     {
         goingDown = true;
         goingUp = false;
+
+        if (freezePlayer)
+        {
+            sponge.stop = true;
+            rocky.stop = true;
+            liam.stop = true;
+        }
     }
     public void PilarUp()
     {
         goingDown = false;
         goingUp = true;
+
+        if (freezePlayer)
+        {
+            sponge.stop = true;
+            rocky.stop = true;
+            liam.stop = true;
+        }
     }
-
-
 }
