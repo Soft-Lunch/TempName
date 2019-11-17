25aa8fff28b8139efe0812114bbf4a7ecfecd864
Aleix Gabarró <aleix.gab@gmail.com>
2019-11-17 10:09:22 +0100

    Menu

--- TempName/Assets/Scripts/UIScript.cs
+++ TempName/Assets/Scripts/UIScript.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class UIScript: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject backgroundMenu;
+    public GameObject backgroundCredits;
 
+    public string nameScene;
     // Update is called once per frame
     void Update()
     {
-        
+        var gamePad = Gamepad.current;
+
+        if (gamePad != null)
+        {
+            if (backgroundMenu.activeInHierarchy)
+            {
+                if (gamePad.buttonWest.wasPressedThisFrame)
+                    Quit();
+                else if (gamePad.buttonNorth.wasPressedThisFrame)
+                    ChangeSceneByName(nameScene);
+                else if (gamePad.buttonEast.wasPressedThisFrame)
+                    Credits();
+            }
+
+            else
+            {
+                if (gamePad.buttonEast.wasPressedThisFrame)
+                    Menu();
+            }
+        }
     }
 
     public void ChangeSceneByName(string name)
@@ -25,5 +43,17 @@ public class UIScript: MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        backgroundMenu.SetActive(false);
+        backgroundCredits.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        backgroundMenu.SetActive(true);
+        backgroundCredits.SetActive(false);
     }
 }
